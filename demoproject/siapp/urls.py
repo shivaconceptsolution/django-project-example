fd70ff5f2fde5395527deec02958b36c332b1914
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2024-09-30 11:06:31 +0530

    Add files via upload

--- demoproject/siapp/urls.py
+++ demoproject/siapp/urls.py
@@ -1,5 +1,8 @@
 from . import views
 from django.urls import path
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('hello',views.welcome,name='testfun'),
+    path('radioexample',views.radioexample,name='radioexample'),
+    path('checkboxexample',views.checkboxexample,name='checkboxexample')
 ]
--- demoproject/siapp/views.py
+++ demoproject/siapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 def index(request):
     if request.method=="POST":
@@ -9,3 +9,19 @@ def index(request):
         si = (float(p)*float(r)*float(t))/100
         return HttpResponse("result is "+str(si))
     return render(request,"siapp/index.html")
+def welcome(request):
+    return redirect("index")
+
+def radioexample(request):
+    if request.method=="POST":
+        coursename=request.POST.get("course")
+        return render(request,"siapp/course.html",{"key":coursename})
+    return render(request,"siapp/course.html")
+def checkboxexample(request):
+    if request.method=="POST":
+        courselist = request.POST.getlist("course")
+        data=''
+        for c in courselist:
+            data+=c+' '
+        return render(request,"siapp/checkboxexample.html",{"key":data})
+    return render(request,"siapp/checkboxexample.html")
