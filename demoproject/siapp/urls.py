c1e8825a7fcebe0aa09a82079e4e1cc09d79c5be
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2024-10-02 11:05:03 +0530

    Add files via upload

--- demoproject/siapp/urls.py
+++ demoproject/siapp/urls.py
@@ -4,5 +4,9 @@ urlpatterns=[
     path('',views.index,name='index'),
     path('hello',views.welcome,name='testfun'),
     path('radioexample',views.radioexample,name='radioexample'),
-    path('checkboxexample',views.checkboxexample,name='checkboxexample')
+    path('checkboxexample',views.checkboxexample,name='checkboxexample'),
+    path('colorexample',views.colorexample,name='colorexample'),
+    path('feesexample',views.feesexample,name='feesexample'),
+    path('listexample',views.listexample,name='listexample'),
+    path('ddlexample',views.ddlexample,name='ddlexample')
 ]
--- demoproject/siapp/views.py
+++ demoproject/siapp/views.py
@@ -23,5 +23,36 @@ def checkboxexample(request):
         data=''
         for c in courselist:
             data+=c+' '
-        return render(request,"siapp/checkboxexample.html",{"key":data})
+        return render(request,"siapp/checkboxexample.html",{"key":data,"key1":courselist})
     return render(request,"siapp/checkboxexample.html")
+def colorexample(request):
+    if request.method=="POST":
+        color = request.POST.get("color")
+        return render(request,"siapp/colorexample.html",{"key":color})
+    return render(request,"siapp/colorexample.html")
+def feesexample(request):
+    if request.method=="POST":
+        bfee = request.POST.get("bfee")
+        afee = request.POST.getlist("afee")
+        coursename=''
+        fee=0
+        bcourse=bfee.split(':') #["c","2000"]
+        coursename+=bcourse[0] + " "
+        fee+=int(bcourse[1])
+        for data in afee:
+            acourse=data.split(':')
+            coursename+=acourse[0] + " "
+            fee+=int(acourse[1])
+        return render(request,"siapp/feesexample.html",{"key1":coursename,"key2":fee})
+    return render(request,"siapp/feesexample.html")
+def listexample(request):
+    if request.method=="POST":
+         data = request.POST.getlist("lst")
+         s=''
+         for item in data:
+            s=s+item+" "
+         return render(request,"siapp/listexample.html",{"key":s,"key1":data})
+    return render(request,"siapp/listexample.html")
+def ddlexample(request):
+    data = request.POST.get("ddl")
+    return render(request,"siapp/ddlexample.html",{"key":data})
